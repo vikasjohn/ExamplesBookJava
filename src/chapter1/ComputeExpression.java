b8105424b9d60272571c0c8f6e9bdbfdfe43c22a
vikasjohn <vikas.john@outlook.com>
2017-02-15 22:56:47 +0000

    .

--- src/chapter1/ComputeExpression.java
+++ src/chapter1/ComputeExpression.java
@@ -3,7 +3,7 @@ package chapter1;
 public class ComputeExpression {
 	public static void main(String[] args){
 		System.out.println((10.5 + 2 * 3)/ (45-3.5));
-		System.out.println("3.5*4/2-2.5 is");
+		System.out.println("3.5*4/2-2.5 is the answer!");
 		System.out.println(3.5 * 4 /2 - 2.5);
 	}
 }
